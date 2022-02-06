49f7bd1c4b80678751fbf1ac656fa8a52886597b
Devyn Cyphers <devyncyphers@gmail.com>
2022-02-06 01:40:36 -0700

    log

--- api/Program.cs
+++ api/Program.cs
@@ -17,6 +17,8 @@ builder.Services.Configure<RouteOptions>(options => {
   options.LowercaseQueryStrings = true;
 });
 
+Console.WriteLine("Connection string: " + builder.Configuration.GetConnectionString("DefaultConnection"));
+
 builder.Services.AddHealthChecks();
 
 // Add services to the container.
